afbd9760cebeb3972680dea673a74a182853a0b8
Bagaskara.2004 <126261494+bagaskara2004@users.noreply.github.com>
2023-06-10 21:16:06 +0800

    update index.php

--- index.php
+++ index.php
@@ -3,59 +3,70 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Landing | page</title>
+	<title>HOME | page</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Mulish&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
-	<header>
-		<h1>netclip</h1>
-		<nav>
-			<a href="Login/signIn.php">signIn</a>
-			<a href="Login/signUp.php">signUp</a>
-		</nav>
-	</header>
-	<div class="container">
+<header id="home">
+	<nav>
 		<div class="logo">
-			<h1>NETCLIP</h1>
+			<div class="bulat"><i class="bi bi-film"></i></div>
+			<h5>Netclip</h5>
 		</div>
-		<div id="about">
-			<label>ABOUT</label>
-			<p>Netclip ini adalah sebuah website yang menyediakan tayangan film, dengan tujuan supaya semua orang bisa menonton film dimana pun dan kapan pun, kami menawarkan semua genre film. jika ingin menonton sebuah film maka anda harus berlangganan terlebih dahulu supaya bisa menggunakan fitur yang tersedia</p>
+		<ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#genre">Genre</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+	</nav>
+	<div class="teks">
+		<h1>SELAMAT DATANG DI NETCLIP</h1>
+		<p>mau nonton film anti ribet? <strong>disini aja!!</strong></p>
+		<a href="Login/signin.php">Join Now<i class="bi bi-arrow-return-left arah-kiri"></i></a>
+		<i class="bi bi-chevron-double-down arah-bawah"></i>
+	</div>
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#151313" fill-opacity="1" d="M0,288L15,245.3C30,203,60,117,90,106.7C120,96,150,160,180,165.3C210,171,240,117,270,101.3C300,85,330,107,360,138.7C390,171,420,213,450,208C480,203,510,149,540,112C570,75,600,53,630,58.7C660,64,690,96,720,128C750,160,780,192,810,186.7C840,181,870,139,900,154.7C930,171,960,245,990,245.3C1020,245,1050,171,1080,138.7C1110,107,1140,117,1170,128C1200,139,1230,149,1260,149.3C1290,149,1320,139,1350,149.3C1380,160,1410,192,1425,208L1440,224L1440,0L1425,0C1410,0,1380,0,1350,0C1320,0,1290,0,1260,0C1230,0,1200,0,1170,0C1140,0,1110,0,1080,0C1050,0,1020,0,990,0C960,0,930,0,900,0C870,0,840,0,810,0C780,0,750,0,720,0C690,0,660,0,630,0C600,0,570,0,540,0C510,0,480,0,450,0C420,0,390,0,360,0C330,0,300,0,270,0C240,0,210,0,180,0C150,0,120,0,90,0C60,0,30,0,15,0L0,0Z"></path></svg>
+</header>
+<div id="about">
+	<h1>ABOUT</h1>
+	<p>Netclip adalah sebuah website yang menyediakan berbagai macam film, kamu bisa menontonnya dimana saja dan kapan pun yang kamu mau yang pastinya tidak ribet. disini kamu bisa melihat film yang sedang populer dan kamu juga bisa menambahkannya ke favorite agar bisa menontonnya kembali tanpa harus mencari filmnya. tunggu apa lagi ayo <a href="#home">join sekarang</a></p>
+</div>
+<div id="genre">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#605E5E" fill-opacity="1" d="M0,192L10.4,208C20.9,224,42,256,63,266.7C83.5,277,104,267,125,250.7C146.1,235,167,213,188,170.7C208.7,128,230,64,250,48C271.3,32,292,64,313,96C333.9,128,355,160,376,165.3C396.5,171,417,149,438,149.3C459.1,149,480,171,501,160C521.7,149,543,107,563,85.3C584.3,64,605,64,626,106.7C647,149,668,235,689,261.3C709.6,288,730,256,751,208C772.2,160,793,96,814,101.3C834.8,107,856,181,877,192C897.4,203,918,149,939,128C960,107,981,117,1002,154.7C1022.6,192,1043,256,1064,245.3C1085.2,235,1106,149,1127,133.3C1147.8,117,1169,171,1190,170.7C1210.4,171,1231,117,1252,112C1273,107,1294,149,1315,149.3C1335.7,149,1357,107,1377,85.3C1398.3,64,1419,64,1430,64L1440,64L1440,0L1429.6,0C1419.1,0,1398,0,1377,0C1356.5,0,1336,0,1315,0C1293.9,0,1273,0,1252,0C1231.3,0,1210,0,1190,0C1168.7,0,1148,0,1127,0C1106.1,0,1085,0,1064,0C1043.5,0,1023,0,1002,0C980.9,0,960,0,939,0C918.3,0,897,0,877,0C855.7,0,835,0,814,0C793,0,772,0,751,0C730.4,0,710,0,689,0C667.8,0,647,0,626,0C605.2,0,584,0,563,0C542.6,0,522,0,501,0C480,0,459,0,438,0C417.4,0,397,0,376,0C354.8,0,334,0,313,0C292.2,0,271,0,250,0C229.6,0,209,0,188,0C167,0,146,0,125,0C104.3,0,83,0,63,0C41.7,0,21,0,10,0L0,0Z"></path></svg>
+	<h1>GENRE</h1>
+	<div class="list">
+		<div class="film action">
+			<p><strong>Genre Action </strong>ini adalah genre yang paling disukai oleh anak muda genre ini juga punya banyak aksi seperti fighting, tembak-tembakan, dan lainnya ayo tonton hanya di netclip</p>
+			<div class="poster"></div>
 		</div>
-		<div id="genre">
-			<div class="list action">
-				<div class="info">
-					<h1>Action</h1>
-					<p>tonton film dengan tema action, yang pasti menyenangkan dan sangat keren jangan sampai tidak nonton ya!!</p>
-				</div>
-				<div class="img"></div>
-			</div>
-			<div class="list romantis">
-				<div class="img"></div>
-				<div class="info">
-					<h1>Romantis</h1>
-					<p>genre penuh percintaan, yang bisa bikin hati terharu, tonton segera genre ini!!</p>
-				</div>
-			</div>
-			<div class="list komedi">
-				<div class="info">
-					<h1>Komedi</h1>
-					<p>tonton film komedi, bisa bikin sakit perut karena tertawa, jangan larut dalam kesedihan, tonton ini saja dijamin heppii</p>
-				</div>
-				<div class="img"></div>
-			</div>
-			<div class="list horor">
-				<div class="img"></div>
-				<div class="info">
-					<h1>Horor</h1>
-					<p>ingin merasakan sensasi yang bisa bikin merinding, genre ini sangat cocok buat kamu yang suka dengan mistis dan hal hal goib, yuk tonton segera !!!</p>
-				</div>
-			</div>
+		<div class="film comedy">
+			<div class="poster"></div>
+			<p><strong>Genre Comedy </strong>tontonan yang bikin kita happy bisa bikin mood lebih baik dan juga ceria, dengan penuh canda tawa yang ada di genre ini kalian bisa tertawa terbahak bahak, ayo tonton sekarang dan tertawalah sebelum tertawa itu dilarang </p>
+		</div>
+		<div class="film horor">
+			<p><strong>Genre Horor </strong>genre yang penuh dengan hal hal mistis dan goib, genre ini bisa bikin merinding, siapa saja yang menontonnya pasti ketakutan, kalau kalian penakut jangan tonton ini !!</p>
+			<div class="poster"></div>
+		</div>
+		<div class="film romantis">
+			<div class="poster"></div>
+			<p><strong>Genre Romantis </strong>tontonan yang mengharukan penuh dengan kasih sayang yang pastinya dramatis, genre yang paling disukai di kalangan gadis gadis remaja, jangan lupa ditonton ya!!</p>
 		</div>
 	</div>
-	<footer>
-		<p>Email : netclip@gmail.com</p>
-		<p>tonton film kesukaanmu hanya di netclip</p>
-	</footer>
+</div>
+<div id="contact">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#151313" fill-opacity="1" d="M0,64L6.2,74.7C12.3,85,25,107,37,144C49.2,181,62,235,74,224C86.2,213,98,139,111,138.7C123.1,139,135,213,148,240C160,267,172,245,185,202.7C196.9,160,209,96,222,80C233.8,64,246,96,258,133.3C270.8,171,283,213,295,224C307.7,235,320,213,332,181.3C344.6,149,357,107,369,80C381.5,53,394,43,406,69.3C418.5,96,431,160,443,165.3C455.4,171,468,117,480,106.7C492.3,96,505,128,517,154.7C529.2,181,542,203,554,186.7C566.2,171,578,117,591,122.7C603.1,128,615,192,628,213.3C640,235,652,213,665,192C676.9,171,689,149,702,138.7C713.8,128,726,128,738,144C750.8,160,763,192,775,176C787.7,160,800,96,812,64C824.6,32,837,32,849,48C861.5,64,874,96,886,112C898.5,128,911,128,923,154.7C935.4,181,948,235,960,229.3C972.3,224,985,160,997,117.3C1009.2,75,1022,53,1034,53.3C1046.2,53,1058,75,1071,122.7C1083.1,171,1095,245,1108,240C1120,235,1132,149,1145,144C1156.9,139,1169,213,1182,224C1193.8,235,1206,181,1218,176C1230.8,171,1243,213,1255,218.7C1267.7,224,1280,192,1292,154.7C1304.6,117,1317,75,1329,64C1341.5,53,1354,75,1366,80C1378.5,85,1391,75,1403,106.7C1415.4,139,1428,213,1434,250.7L1440,288L1440,0L1433.8,0C1427.7,0,1415,0,1403,0C1390.8,0,1378,0,1366,0C1353.8,0,1342,0,1329,0C1316.9,0,1305,0,1292,0C1280,0,1268,0,1255,0C1243.1,0,1231,0,1218,0C1206.2,0,1194,0,1182,0C1169.2,0,1157,0,1145,0C1132.3,0,1120,0,1108,0C1095.4,0,1083,0,1071,0C1058.5,0,1046,0,1034,0C1021.5,0,1009,0,997,0C984.6,0,972,0,960,0C947.7,0,935,0,923,0C910.8,0,898,0,886,0C873.8,0,862,0,849,0C836.9,0,825,0,812,0C800,0,788,0,775,0C763.1,0,751,0,738,0C726.2,0,714,0,702,0C689.2,0,677,0,665,0C652.3,0,640,0,628,0C615.4,0,603,0,591,0C578.5,0,566,0,554,0C541.5,0,529,0,517,0C504.6,0,492,0,480,0C467.7,0,455,0,443,0C430.8,0,418,0,406,0C393.8,0,382,0,369,0C356.9,0,345,0,332,0C320,0,308,0,295,0C283.1,0,271,0,258,0C246.2,0,234,0,222,0C209.2,0,197,0,185,0C172.3,0,160,0,148,0C135.4,0,123,0,111,0C98.5,0,86,0,74,0C61.5,0,49,0,37,0C24.6,0,12,0,6,0L0,0Z"></path></svg>
+	<h1>CONTACT</h1>
+	<ul>
+		<li class="facebook"><a href="#"></a></li>
+		<li class="gmail"><a href="#"></a></li>
+		<li class="twitter"><a href="#"></a></li>
+		<li class="instagram"><a href="#"></a></li>
+	</ul>
+</div>
 </body>
 </html>
