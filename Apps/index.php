6d9b546216984441850f5db20eb91bb940528359
Bagaskara.2004 <126261494+bagaskara2004@users.noreply.github.com>
2023-06-26 08:37:54 +0800

    update bug jpg

--- Apps/index.php
+++ Apps/index.php
@@ -82,7 +82,7 @@
 	<div class="list" id="list">
 		<?php while ($datasFilm = mysqli_fetch_assoc($dataFilm)) {?>
 		<div class="card">
-			<div class="img" style="background-image: url('Poster/<?=$datasFilm['poster'];?>.jpg');"><p><?=$datasFilm['poster']; ?></p></div>
+			<div class="img" style="background-image: url('Poster/<?=$datasFilm['poster'];?>');"><p><?=$datasFilm['poster']; ?></p></div>
 			<form action="PemutarVideo/index.php" method="post">
 				<input type="hidden" name="id" value="<?= $datasUser['id'] ?>">
 				<button name="submit"value="<?=$datasFilm['id'] ?>">Detail</button>
