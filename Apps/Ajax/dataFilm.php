42dbc156c1ddf54b59d786c8a695ce2afc986053
Bagaskara.2004 <126261494+bagaskara2004@users.noreply.github.com>
2023-06-26 09:00:56 +0800

    update ajax

--- Apps/Ajax/dataFilm.php
+++ Apps/Ajax/dataFilm.php
@@ -5,7 +5,7 @@ $dataFilm = mysqli_query($koneksi,"SELECT * FROM listfilm WHERE poster LIKE '%$s
 ?>
 <?php while ($datasFilm = mysqli_fetch_assoc($dataFilm)) {?>
 	<div class="card">
-		<div class="img" style="background-image: url('Poster/<?=$datasFilm['poster'];?>.jpg');"><p><?=$datasFilm['poster']; ?></p></div>
+		<div class="img" style="background-image: url('Poster/<?=$datasFilm['poster'];?>');"><p><?=$datasFilm['poster']; ?></p></div>
 		<form action="PemutarVideo/index.php" method="post">
 			<input type="hidden" name="id" value="<?= $datasUser['id'] ?>">
 			<button name="submit"value="<?=$datasFilm['id'] ?>">Detail</button>
